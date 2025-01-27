a4d5477c3bb30bb2316b28a6cb25ddff1fd7a372
Dan Rasmuson <danrasmuson@gmail.com>
2025-01-27 12:34:42 -0600

    Adding code for Humata api key

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const dotenv = require('dot');
 const path = require('path');
+dotenv()
 const app = express();
 const port = 3000;
+
+console.log(process.env.HUMATA_API_KEY)
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
